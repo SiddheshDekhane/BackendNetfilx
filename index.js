e4f7d6e4570938c9fad161355d86b9ed4f8bbf0b
SiddheshDekhane <161200606+SiddheshDekhane@users.noreply.github.com>
2024-10-01 21:56:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,11 +17,8 @@ const app = express();
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(cookieParser());
-const corsOptions = {
-    origin: ['http://localhost:3000', 'https://frontendnetfix.onrender.com/'],
 
-};
-app.use(cors(corsOptions));
+app.use(cors());
  
 // api
 app.use("/api/v1/user", userRoute);
